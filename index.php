018d94ab174d116f346d1906710a92feee5e22c8
dneb <ya.dneb@yandex.ru>
2020-10-04 21:39:50 +0300

    index

--- index.php
+++ index.php
@@ -12,54 +12,11 @@
 </head>
 <body>
 
-<div class="chat-box">
-    <div class="messeges-fild">
-        <div class="messege-box">
-            <div class="sender">Qlarmv:</div>
-            <div class="messege">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores aut consequatur itaque iure, laudantium omnis pariatur! Atque beatae culpa cum eveniet fuga incidunt minus nostrum, quas quod recusandae tempora!
-            </div>
-        </div>
-        <div class="messege-box" style="align-self: end; background-color: aquamarine">
-            <div class="sender">You:</div>
-            <div class="messege">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, repellendus?
-            </div>
-        </div>
-        <div class="messege-box">
-            <div class="sender">Qlarmv:</div>
-            <div class="messege">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias aut beatae est, inventore ipsum laborum libero, necessitatibus non nulla possimus repellendus vitae. Eius, maxime sunt?
-            </div>
-        </div>
-        <div class="messege-box">
-            <div class="sender">Qlarmv:</div>
-            <div class="messege">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            </div>
-        </div>
-        <div class="messege-box">
-            <div class="sender">Qlarmv:</div>
-            <div class="messege">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae delectus dolorem magnam natus ut.
-            </div>
-        </div>
-        <div class="messege-box" style="align-self: end; background-color: aquamarine">
-            <div class="sender">You:</div>
-            <div class="messege">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda necessitatibus non quis? A corporis dolorem fuga fugiat labore laudantium necessitatibus nesciunt obcaecati officiis, quam quis recusandae reprehenderit sit veniam.
-            </div>
-        </div>
-    </div>
-
-    <div class="caht-fild">
-        <textarea class="input-text"></textarea>
-        <a class="btn"> Отправить </a>
-    </div>
-</div>
+<?require_once ''?>
 
 </body>
 </html>
 
+<script src="js/jquery-3.5.1.min.js"></script>
 <script src="https://kit.fontawesome.com/d331940b91.js" crossorigin="anonymous"></script>
 <script src="js/main.js"></script>
